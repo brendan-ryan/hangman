27b9ad977b909ab29cb3f415e96bfb573165691e
brendan-ryan <brendanmryan@gmail.com>
2022-09-20 14:09:44 -0600

    Build Hangman game file

---
+++ Hangman.py
@@ -0,0 +1,64 @@
+word = "PERSEVERE"
+
+print(word[0])
+
+letter = 'S'
+test_list_length = len(word)
+
+print(test_list_length)
+
+counter = 0
+test_list = ["_", "_"]
+test_list.append("S")
+test_list.append("R")
+
+print("List includes:", test_list)
+
+print("Test List Length is", test_list_length)
+
+print("Counter is", counter)
+
+for i in range(5):
+    print("Rawr")
+
+print(len(word)-1)
+print(type(test_list))
+
+
+for i in range(len(word)-1):
+    test_list.append("F")
+
+
+while counter < (test_list_length - 1):
+    test_list.append("R")
+
+
+print(test_list)
+print("End")
+
+"""
+counter = 0
+while counter < (len(word)-1):
+    if letter == word[counter]:
+        character_positions.append
+
+character_positions = 
+
+guessed_correctly = [0, 1, 6, 2]
+guessed_incorrectly = []
+
+guessed_correctly.sort()
+print(guessed_correctly)
+
+
+
+
+for i in range (len(word)-1):
+    print("_ ")
+
+def get_letter():
+    enter_letter = input("Guess a letter")
+    if enter_letter
+
+
+"""
